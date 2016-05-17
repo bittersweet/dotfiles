46c64c8c07005ae1ef4002e2f834e189c3395fd8
Mark Mulder <markmulder@gmail.com>
2016-05-17 14:02:23 +0200

    Add setup.sh to copy all files

---
+++ setup.sh
@@ -0,0 +1,13 @@
+ln -s ~/dotfiles/zsh ~/.zsh
+ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/ackrc ~/.ackrc
+ln -s ~/dotfiles/gemrc ~/.gemrc
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/gitignore ~/.gitignore
+ln -s ~/dotfiles/irbrc ~/.irbrc
+ln -s ~/dotfiles/rspec ~/.rspec
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/vim ~/.vim
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/spacemacs ~/.spacemacs
+echo "Files linked"
